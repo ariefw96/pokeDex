fda3dcedd6e3fb3923872c756a6d3b5b26d32a3b
Arif Nur Agung <62019667+agungl4@users.noreply.github.com>
2021-03-25 18:17:15 +0900

    pagination

--- src/pages/details/index.js
+++ src/pages/details/index.js
@@ -1,19 +1,23 @@
 import React, { Component } from 'react';
-
+import './style.css';
+import Navbar from '../../components/navbar';
 class Details extends Component {
     state = {
         pokemon: {},
+        isLoading: true,
     }
 
     getSinglePoke = () => {
         const { match } = this.props;
         const newUrl = match.params.name
         fetch(`https://pokeapi.co/api/v2/pokemon/${newUrl}`)
-            .then(({ data }) => {
-                console.log(data)
+            .then(data =>
+                data.json())
+            .then(response => {
                 this.setState({
-                    pokemon: data,
-                })
+                    pokemon: response,
+                    isLoading: false,
+                });
             })
             .catch((err) => {
                 console.log(err)
@@ -27,26 +31,41 @@ class Details extends Component {
     render() {
         const { pokemon } = this.state
         console.log(this.state)
+        let status
+        if (this.state.isLoading != true) {
+            status =
+                <>
+                    <section className="detail-view" >
+                        <div className='data-wrapper'>
+                            <div className='imgWrap'>
+                                <img alt="pokemon" src={`https://img.pokemondb.net/artwork/large/${this.props.match.params.name}.jpg`} />
+                            </div>
+                            <p className='data-name'>ID : {pokemon.id} </p>
+                            <p className='data-name'>Name : {pokemon.name}</p>
+                            {
+                                pokemon.types.map(({ type, index }) => {
+                                    if (index != 0) {
+                                    return (
+                                        <>
+                                            <p className="data-name">Type : {type.name}</p>
+                                        </>
+                                    )}
+                                })
+                            }
+                        </div>
+                    </section>
+                </>
+        } else {
+            <h1>Loading</h1>
+        }
         return (
-            <section className="detail-view" >
-                <div className='data-wrapper'>
-                    {
-                        pokemon && pokemon.map(({ id, name, type, sprite }) => {
-                            return (
-                                <>
-                                    <img src={sprite.front_default} className='sprite-image' alt="sprite" />
-                                    <h1 className='data-name'>ID: {id} {name}</h1>
-                                    <p className="data-char">Type: {type}</p>
-                                </>
-                            )
-                        })
-                    }
-
-
-                </div>
-            </section>
+            <>
+                <Navbar />
+                { status}
+            </>
         );
     }
 }
+
 
 export default Details;
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import Navbar from '../../components/navbar';
 import CardList from '../../components/card'
-
+import axios from 'axios'
+import './../style.css'
+import Button from 'react-bootstrap/Button';
 
 class home extends Component {
     constructor() {
@@ -9,27 +11,91 @@ class home extends Component {
 
         this.state = {
             pokemons: [],
+            nextpage: '',
+            prevpage: '',
         };
     }
 
     componentDidMount() {
-        fetch('https://pokeapi.co/api/v2/pokemon?limit=151')
-            .then(response => response.json())
-            .then(name => this.setState({ pokemons: name.results }));
+        axios.get('https://pokeapi.co/api/v2/pokemon?limit=20')
+            .then(({ data }) => {
+                this.setState({
+                    pokemons: data.results,
+                    prevpage: data.previous,
+                    nextpage: data.next,
+                })
+                console.log(this.state.prevpage)
+                console.log(this.state.nextpage)
+                console.log(this.state.pokemons)
+            }).catch((error) => {
+                console.log(error)
+            })
     }
+
+
+    nextPage() {
+        // const nextPage = this.state.nextpage
+        console.log(this.state.nextpage)
+        // if (nextPage != null) {
+        //     axios.get(this.state.nextpage)
+        //         .then(({ data }) => {
+        //             this.setState({
+        //                 pokemons: data.results,
+        //                 prevpage: data.previous,
+        //                 nextpage: data.next,
+        //             })
+        //         }).catch((error) => {
+        //             console.log(error)
+        //         })
+        // }
+    }
+
+    prevPage = () => {
+        const prevPage = this.state.prevpage
+        if (prevPage != null) {
+            axios.get(prevPage)
+                .then(({ data }) => {
+                    this.setState({
+                        pokemons: data.results,
+                        prevpage: data.previous,
+                        nextpage: data.next,
+                    })
+                }).catch((error) => {
+                    console.log(error)
+                })
+        }
+    }
+
+    //     fetch('https://pokeapi.co/api/v2/pokemon?limit=151')
+    //         .then(response => response.json())
+    //         .then(name => this.setState({ pokemons: name.results }));
+    // }
     render() {
-        const {pokemons} = this.state
+        const { pokemons, nextpage, prevpage } = this.state
         // console.log(this.state.pokemons)
         return (
             <>
                 <Navbar />
+                <button className='btn-primary'
+                    onClick={this.prevPage}
+                >
+                    <p style={{ paddingHorizontal: 10 }}>{`<< `}Prev</p>
+                </button>
+
+                <button className='btn-primary'
+                    onClick={this.nextPage}
+                >
+                    <p style={{ paddingHorizontal: 10 }}>{` >>`}Next</p>
+                </button>
+
                 {
-                            pokemons && pokemons.map(({ id, name, url }) => {
-                                return (
-                                    <CardList key={id} name={name} url={url} />
-                                )
-                            })
-                        }
+                    pokemons && pokemons.map(({ id, name, url }) => {
+                        return (
+                            <CardList key={id} name={name} url={url} />
+                        )
+                    })
+                }
+
             </>
         );
     }
